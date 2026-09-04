86c3f529c492369e749e825a13d6b20974e8d39e
Kapil Gaire <eriaglipak312@gmail.com>
2026-09-04 15:35:42 +0545

    add: integers are used to represnt opcodes

--- backend/os_engine/isa.py
+++ backend/os_engine/isa.py
@@ -3,15 +3,16 @@
 """
 
 from dataclasses import dataclass
-from enum import Enum, auto
+from enum import IntEnum
 
 
-class Opcode(Enum):
-    ADD = auto()
-    HALT = auto()
-    SUBTRACT = auto()
-    JMP = auto()  # unconditional jump
-    JZ = auto()  # jump on zero
+class Opcode(IntEnum):
+    # 6 bit opcode will be used. so 64 posisble opcodes (0x00 to 0x3F)
+    ADD = 0x00
+    HALT = 0x01
+    SUBTRACT = 0x02
+    JMP = 0x03  # unconditional jump
+    JZ = 0x04  # jump on zero
 
 
 @dataclass()
